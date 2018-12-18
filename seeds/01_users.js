d6431a4f043ed38b312627e4982b9fd635a55d5c
xwrexx <wrex1285@gmail.com>
2018-12-18 10:57:12 -0700

    seeds for users created

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -5,9 +5,38 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {
+          userName: 'WReX',
+          firstName: 'Jess',
+          lastName: 'Markgraf',
+          Age: 34,
+          vehicle: 1,
+          trips: 1
+        },
+        {
+          userName: 'NewCarSmell',
+          firstName: 'Josh',
+          lastName: 'Scala',
+          Age: 24,
+          vehicle: 2,
+          trips: 2
+        },
+        {
+          userName: 'EssssGetit',
+          firstName: 'Ryan',
+          lastName: 'StGermain',
+          Age: 20,
+          vehicle: 3,
+          trips: 3
+        },
+        {
+          userName: 'ThatTallDude',
+          firstName: 'Trey',
+          lastName: 'Young',
+          Age: 29,
+          vehicle: 4,
+          trips: 4
+        }
       ]);
     });
 };
