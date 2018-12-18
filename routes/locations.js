e03d9f91875f077ca7d8f5cb7778a4e52e40b14c
thatnewcarsmell <scalajr@gmail.com>
2018-12-18 12:15:48 -0700

    coordinating link between routes and queries files

--- routes/locations.js
+++ routes/locations.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const queries = require('../queries/trips_queries.js')
+const queries = require('../queries/locations_queries.js')
 
 module.exports = router
