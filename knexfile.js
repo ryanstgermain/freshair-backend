b63c8760f433003b5026e217c214fe9a8e4ff03c
xwrexx <wrex1285@gmail.com>
2018-12-18 10:26:34 -0700

    updated knex file

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,15 @@
-// require('dotenv').config()
+// Update with your config settings.
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      database: 'freshair_db',
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS
-    }
+    connection: 'postgresql://localhost/freshair_db'
   },
-  
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
   }
-  
 
 };
