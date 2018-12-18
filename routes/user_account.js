0538b3ad98d1549ca41293a43f1f1ee6359ca984
= <treyyoung8@gmail.com>
2018-12-18 12:02:19 -0700

    added user account queries codes and did basic layout for user account route file

---
+++ routes/user_account.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const router = express.Router()
+const queries = require('../queries/user_account_queries')
+
+module.exports = router
--- seeds/04_user_account.js
+++ seeds/04_user_account.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('user_account').del()
+  return knex('user_accounts').del()
     .then(function () {
       // Inserts seed entries
-      return knex('user_account').insert([
+      return knex('user_accounts').insert([
         {
           trip_id: 1,
           amount: 14.00,
