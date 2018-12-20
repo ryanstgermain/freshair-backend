61aa6c6de2e8dc73cbc168a37b22f185104701a9
thatnewcarsmell <scalajr@gmail.com>
2018-12-20 09:44:59 -0700

    changes to the data in order to accommodate decreased time to implement

--- migrations/20181218103522_locations.js
+++ migrations/20181218103522_locations.js
@@ -3,7 +3,6 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('locations', location => {
     location.increments('id')
     location.string('resort_name')
-    location.text('location_url')
     location.integer('base_elevation')
     location.integer('summit_elevation')
     location.text('rental_url')
